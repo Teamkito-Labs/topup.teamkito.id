27134233d9f1dc84793c8b115832f0ab7f0f050e
Iit Pranata <iitpranataip@gmail.com>
2024-07-05 12:01:00 +0700

    fix: tambah perintah hapus

--- resources/views/Pemilik/partials/tambah-kategori.blade.php
+++ resources/views/Pemilik/partials/tambah-kategori.blade.php
@@ -1,30 +1,29 @@
-<div class="col-5">
-    <div class="card shadow-lg rounded-lg height-card box-margin mx-0 px-0">
-        <div class="card-body">
-            <h3 class="card-title mb-4">Tambah Kategori</h3>
-            <form action="{{ route('kategori.produk.store') }}" method="post">
-                @csrf
-                <div class="form-group">
-                    <label for="exampleFormControlSelect1">Produk</label>
-                    <select class="form-control" id="exampleFormControlSelect1">
-                      <option>1</option>
-                      <option>2</option>
-                      <option>3</option>
-                      <option>4</option>
-                      <option>5</option>
-                    </select>
-                </div>
-                <div class="form-group">
-                    <label for="nama_produk">Nama Produk</label>
-                    <input type="text" id="nama_produk"
-                        class="form-control @error('nama_produk') is-invalid @enderror" name="nama_produk"
-                        placeholder="Nama Produk" required>
-                </div>
-                <div class="d-flex justify-content-end">
-                    <button @click="window.history.back()" class="btn btn-secondary btn-rounded btn-sm mr-2">Batal</button>
-                    <button type="submit" class="btn btn-primary btn-rounded btn-sm">Simpan</button>
-                </div>
-            </form>
-        </div>
+
+<div class="card shadow-lg rounded-lg height-card box-margin mx-0 px-0">
+    <div class="card-body">
+        <h3 class="card-title mb-4">Tambah Kategori</h3>
+        <form action="{{ route('kategori.produk.store') }}" method="post">
+            @csrf
+            <div class="form-group">
+                <label for="exampleFormControlSelect1">Produk</label>
+                <select class="form-control" id="exampleFormControlSelect1">
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="nama_produk">Nama Produk</label>
+                <input type="text" id="nama_produk"
+                    class="form-control @error('nama_produk') is-invalid @enderror" name="nama_produk"
+                    placeholder="Nama Produk" required>
+            </div>
+            <div class="d-flex justify-content-end">
+                <button @click="window.history.back()" class="btn btn-secondary btn-rounded btn-sm mr-2">Batal</button>
+                <button type="submit" class="btn btn-primary btn-rounded btn-sm">Simpan</button>
+            </div>
+        </form>
     </div>
 </div>
---
+++ resources/views/Pemilik/produk/prabayar.blade.php
@@ -0,0 +1,33 @@
+@section('title', 'Pengaturan Kategori')
+@section('description', 'Top Up game murah dan cepat di topup.teamkito.id! Dukung permainan Anda dengan Top Up untuk
+Free Fire, Mobile Legends, PUBG Mobile, dan Genshin Impact. Proses instan!')
+@section('keywords', 'Top Up game murah, Top Up game cepat, Top Up Free Fire murah, Top Up Mobile Legends cepat, Top Up
+PUBG Mobile murah, Top Up Genshin Impact cepat, Top Up game online, Top Up game Indonesia, beli diamond Free Fire, beli
+diamond Mobile Legends, Top Up UC PUBG murah, Top Up Genesis Crystal Genshin Impact, situs Top Up game terpercaya,
+layanan Top Up game instan, Top Up game resmi, harga Top Up game terjangkau, Top Up game populer, Top Up game aman, top
+up game mobile, Top Up game terbaik
+')
+<x-app-layout>
+    <h3 class="card-title mb-4">Pengaturan Kategori</h3>
+    <div class="row"></div>
+    </div>
+    @push('styles')
+	<link rel="stylesheet" href="{{ url('xvito-responsive-bootstrap/css/default-assets/new/sweetalert-2.min.css') }}">
+    @endpush
+    @push('scripts')
+	<script src="{{ url('xvito-responsive-bootstrap/js/default-assets/sweetalert2.min.js') }}"></script>
+	@if(session()->has('success'))
+		<script>
+			Swal.fire({
+				title: 'Sukses!',
+				text: '{{ session()->get("success") }}',
+				type: 'success',
+				showConfirmButton: false, // Hide the confirm button
+				showCancelButton: false, // Hide the confirm button
+				timer: 1500, // Close automatically after 1500ms (1,5 seconds)
+				allowOutsideClick: false,
+			});
+		</script>
+	@endif
+    @endpush
+</x-app-layout>
