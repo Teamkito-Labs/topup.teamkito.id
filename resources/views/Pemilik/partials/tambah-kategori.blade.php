8bad4af1b3aa5aa5ea9cdf38235d75fd22f09141
Iit Pranata <iitpranataip@gmail.com>
2024-07-05 12:11:54 +0700

    fix: ganti col menjadi responsive

--- resources/views/Pemilik/partials/tambah-kategori.blade.php
+++ resources/views/Pemilik/partials/tambah-kategori.blade.php
@@ -1,115 +1,107 @@
 @if (Request::segment(4) == 'produk')
-<div class="col-5">
-    <div class="card shadow-lg rounded-lg height-card box-margin mx-0 px-0">
-        <div class="card-body">
-            <h3 class="card-title mb-4">Tambah Produk</h3>
-            <form action="{{ route('kategori.produk.store') }}" method="post">
-                @csrf
-                <div class="form-group">
-                    <label for="nama_produk">Nama Produk</label>
-                    <input type="text" id="nama_produk" class="form-control @error('nama_produk') is-invalid @enderror"
-                        name="nama_produk" placeholder="Nama Produk" value="{{ old('nama_produk') }}" required>
-                </div>
-                <div class="d-flex justify-content-end">
-                    <a href="{{ route('kategori') }}" class="btn btn-secondary btn-rounded btn-sm mr-2">Batal</a>
-                    <button type="submit" class="btn btn-primary btn-rounded btn-sm">Simpan</button>
-                </div>
-            </form>
-        </div>
+<div class="card shadow-lg rounded-lg height-card box-margin mx-0 px-0">
+    <div class="card-body">
+        <h3 class="card-title mb-4">Tambah Produk</h3>
+        <form action="{{ route('kategori.produk.store') }}" method="post">
+            @csrf
+            <div class="form-group">
+                <label for="nama_produk">Nama Produk</label>
+                <input type="text" id="nama_produk" class="form-control @error('nama_produk') is-invalid @enderror"
+                    name="nama_produk" placeholder="Nama Produk" value="{{ old('nama_produk') }}" required>
+            </div>
+            <div class="d-flex justify-content-end">
+                <a href="{{ route('kategori') }}" class="btn btn-secondary btn-rounded btn-sm mr-2">Batal</a>
+                <button type="submit" class="btn btn-primary btn-rounded btn-sm">Simpan</button>
+            </div>
+        </form>
     </div>
 </div>
 @elseif (Request::segment(4) == 'kategori')
-<div class="col-5">
-    <div class="card shadow-lg rounded-lg height-card box-margin mx-0 px-0">
-        <div class="card-body">
-            <h3 class="card-title mb-4">Tambah Kategori</h3>
-            <form action="{{ route('kategori.kategori.store') }}" method="post">
-                @csrf
-                <div class="form-group">
-                    <label for="produk_id">Produk</label>
-                    <select class="form-control" id="produk_id" name="produk_id">
-                        <option value="" hidden>-- Pilih Produk --</option>
-                        @forelse ($produk as $item)
-                        <option value="{{ $item->id }}" @if(old('produk_id') == $item->id) selected @endif>{{ $item->nama_produk }}</option>
-                        @empty
+<div class="card shadow-lg rounded-lg height-card box-margin mx-0 px-0">
+    <div class="card-body">
+        <h3 class="card-title mb-4">Tambah Kategori</h3>
+        <form action="{{ route('kategori.kategori.store') }}" method="post">
+            @csrf
+            <div class="form-group">
+                <label for="produk_id">Produk</label>
+                <select class="form-control" id="produk_id" name="produk_id">
+                    <option value="" hidden>-- Pilih Produk --</option>
+                    @forelse ($produk as $item)
+                    <option value="{{ $item->id }}" @if(old('produk_id') == $item->id) selected @endif>{{ $item->nama_produk }}</option>
+                    @empty
 
-                        @endforelse
-                    </select>
-                </div>
-                <div class="form-group">
-                    <label for="nama_kategori">Nama Kategori</label>
-                    <input type="text" id="nama_kategori"
-                        class="form-control @error('nama_kategori') is-invalid @enderror" name="nama_kategori"
-                        placeholder="Nama Kategori" value="{{ old('nama_kategori') }}" required>
-                </div>
-                <div class="d-flex justify-content-end">
-                    <a href="{{ route('kategori') }}" class="btn btn-secondary btn-rounded btn-sm mr-2">Batal</a>
-                    <button type="submit" class="btn btn-primary btn-rounded btn-sm">Simpan</button>
-                </div>
-            </form>
-        </div>
+                    @endforelse
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="nama_kategori">Nama Kategori</label>
+                <input type="text" id="nama_kategori"
+                    class="form-control @error('nama_kategori') is-invalid @enderror" name="nama_kategori"
+                    placeholder="Nama Kategori" value="{{ old('nama_kategori') }}" required>
+            </div>
+            <div class="d-flex justify-content-end">
+                <a href="{{ route('kategori') }}" class="btn btn-secondary btn-rounded btn-sm mr-2">Batal</a>
+                <button type="submit" class="btn btn-primary btn-rounded btn-sm">Simpan</button>
+            </div>
+        </form>
     </div>
 </div>
 @elseif (Request::segment(4) == 'brand')
-<div class="col-5">
-    <div class="card shadow-lg rounded-lg height-card box-margin mx-0 px-0">
-        <div class="card-body">
-            <h3 class="card-title mb-4">Tambah Brand</h3>
-            <form action="{{ route('kategori.brand.store') }}" method="post">
-                @csrf
-                <div class="form-group">
-                    <label for="kategori_id">Kategori</label>
-                    <select class="form-control" id="kategori_id" name="kategori_id">
-                        <option value="" hidden>-- Pilih Kategori --</option>
-                        @forelse ($kategori as $item)
-                        <option value="{{ $item->id }}" @if(old('kategori_id') == $item->id) selected @endif>{{ $item->nama_kategori }}</option>
-                        @empty
+<div class="card shadow-lg rounded-lg height-card box-margin mx-0 px-0">
+    <div class="card-body">
+        <h3 class="card-title mb-4">Tambah Brand</h3>
+        <form action="{{ route('kategori.brand.store') }}" method="post">
+            @csrf
+            <div class="form-group">
+                <label for="kategori_id">Kategori</label>
+                <select class="form-control" id="kategori_id" name="kategori_id">
+                    <option value="" hidden>-- Pilih Kategori --</option>
+                    @forelse ($kategori as $item)
+                    <option value="{{ $item->id }}" @if(old('kategori_id') == $item->id) selected @endif>{{ $item->nama_kategori }}</option>
+                    @empty
 
-                        @endforelse
-                    </select>
-                </div>
-                <div class="form-group">
-                    <label for="nama_brand">Nama Brand</label>
-                    <input type="text" id="nama_brand" class="form-control @error('nama_brand') is-invalid @enderror"
-                        name="nama_brand" placeholder="Nama Brand" value="{{ old('nama_brand') }}" required>
-                </div>
-                <div class="d-flex justify-content-end">
-                    <a href="{{ route('kategori') }}" class="btn btn-secondary btn-rounded btn-sm mr-2">Batal</a>
-                    <button type="submit" class="btn btn-primary btn-rounded btn-sm">Simpan</button>
-                </div>
-            </form>
-        </div>
+                    @endforelse
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="nama_brand">Nama Brand</label>
+                <input type="text" id="nama_brand" class="form-control @error('nama_brand') is-invalid @enderror"
+                    name="nama_brand" placeholder="Nama Brand" value="{{ old('nama_brand') }}" required>
+            </div>
+            <div class="d-flex justify-content-end">
+                <a href="{{ route('kategori') }}" class="btn btn-secondary btn-rounded btn-sm mr-2">Batal</a>
+                <button type="submit" class="btn btn-primary btn-rounded btn-sm">Simpan</button>
+            </div>
+        </form>
     </div>
 </div>
 @elseif (Request::segment(4) == 'tipe')
-<div class="col-5">
-    <div class="card shadow-lg rounded-lg height-card box-margin mx-0 px-0">
-        <div class="card-body">
-            <h3 class="card-title mb-4">Tambah Tipe</h3>
-            <form action="{{ route('kategori.tipe.store') }}" method="post">
-                @csrf
-                <div class="form-group">
-                    <label for="kategori_id">Kategori</label>
-                    <select class="form-control" id="kategori_id" name="kategori_id">
-                        <option value="" hidden>-- Pilih Kategori --</option>
-                        @forelse ($kategori as $item)
-                        <option value="{{ $item->id }}" @if(old('kategori_id') == $item->id) selected @endif>{{ $item->nama_kategori }}</option>
-                        @empty
+<div class="card shadow-lg rounded-lg height-card box-margin mx-0 px-0">
+    <div class="card-body">
+        <h3 class="card-title mb-4">Tambah Tipe</h3>
+        <form action="{{ route('kategori.tipe.store') }}" method="post">
+            @csrf
+            <div class="form-group">
+                <label for="kategori_id">Kategori</label>
+                <select class="form-control" id="kategori_id" name="kategori_id">
+                    <option value="" hidden>-- Pilih Kategori --</option>
+                    @forelse ($kategori as $item)
+                    <option value="{{ $item->id }}" @if(old('kategori_id') == $item->id) selected @endif>{{ $item->nama_kategori }}</option>
+                    @empty
 
-                        @endforelse
-                    </select>
-                </div>
-                <div class="form-group">
-                    <label for="nama_tipe">Nama Tipe</label>
-                    <input type="text" id="nama_tipe" class="form-control @error('nama_tipe') is-invalid @enderror"
-                        name="nama_tipe" placeholder="Nama Tipe" value="{{ old('nama_tipe') }}" required>
-                </div>
-                <div class="d-flex justify-content-end">
-                    <a href="{{ route('kategori') }}" class="btn btn-secondary btn-rounded btn-sm mr-2">Batal</a>
-                    <button type="submit" class="btn btn-primary btn-rounded btn-sm">Simpan</button>
-                </div>
-            </form>
-        </div>
+                    @endforelse
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="nama_tipe">Nama Tipe</label>
+                <input type="text" id="nama_tipe" class="form-control @error('nama_tipe') is-invalid @enderror"
+                    name="nama_tipe" placeholder="Nama Tipe" value="{{ old('nama_tipe') }}" required>
+            </div>
+            <div class="d-flex justify-content-end">
+                <a href="{{ route('kategori') }}" class="btn btn-secondary btn-rounded btn-sm mr-2">Batal</a>
+                <button type="submit" class="btn btn-primary btn-rounded btn-sm">Simpan</button>
+            </div>
+        </form>
     </div>
 </div>
 @endif
--- resources/views/Pemilik/pengaturan/kategori/edit.blade.php
+++ resources/views/Pemilik/pengaturan/kategori/edit.blade.php
@@ -10,7 +10,7 @@ up game mobile, Top Up game terbaik
 <x-app-layout>
     <h3 class="card-title mb-4">Pengaturan Kategori</h3>
     <div class="row">
-		<div class="col-7">
+		<div class="col-12 col-sm-12 col-md-7 col-lg-7">
 			<div class="card shadow-sm rounded-lg height-card box-margin mx-0 px-0">
 				<div class="card-body">
 					<ul class="nav nav-tabs nav-bordered">
@@ -246,7 +246,9 @@ up game mobile, Top Up game terbaik
 				</div>
 			</div>
 		</div>
-		@include('pemilik.partials.edit-kategori')
+		<div class="col-12 col-sm-12 col-md-5 col-lg-5">
+			@include('pemilik.partials.edit-kategori')
+		</div>
     </div>
     @push('styles')
 	<link rel="stylesheet" href="{{ url('xvito-responsive-bootstrap/css/default-assets/new/sweetalert-2.min.css') }}">
